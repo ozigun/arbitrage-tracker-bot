b7a93df6427bb6c1958e9594b8bd9b8f7b7114ce
ozigun <ozigun1988@gmail.com>
2025-07-29 21:08:32 +0300

    feat: responsive style 2

--- app/components/Coin.js
+++ app/components/Coin.js
@@ -35,8 +35,8 @@ export default function Coin({ symbol, price, prevPrice }) {
 
   return (
     <div
-      className={`${bgColor} relative rounded-md w-full max-w-[190px] h-32 p-4 flex flex-col justify-center items-center text-white shadow-md transition-colors duration-300`}>
-      <h3 className="text-lg font-semibold mb-2">{symbol}</h3>
+      className={`${bgColor} relative rounded-md w-48 h-36 p-4 flex flex-col justify-between items-center text-white shadow-md transition-colors duration-300`}>
+      <h3 className="text-lg font-semibold">{symbol}</h3>
       <p className="font-mono text-2xl">{parseFloat(price).toFixed(6)}</p>
 
       {/* Kalp ikonu sağ alt köşede */}
@@ -51,7 +51,12 @@ export default function Coin({ symbol, price, prevPrice }) {
             viewBox="0 0 24 24"
             stroke="red"
             className="w-6 h-6">
-            <path d="M12 21.35l-1.45-1.32C5.4 15.36..." />
+            <path
+              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
+             4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 
+             14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 
+             6.86-8.55 11.54L12 21.35z"
+            />
           </svg>
         ) : (
           <svg
@@ -61,7 +66,14 @@ export default function Coin({ symbol, price, prevPrice }) {
             stroke="currentColor"
             strokeWidth={2}
             className="w-6 h-6">
-            <path d="M12 21.35l-1.45-1.32C5.4 15.36..." />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 
+             4.42 3 7.5 3c1.74 0 3.41 0.81 4.5 2.09C13.09 3.81 
+             14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 
+             6.86-8.55 11.54L12 21.35z"
+            />
           </svg>
         )}
       </button>
